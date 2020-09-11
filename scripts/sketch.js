3d193a7dd6c5c2d02af05306f3d7e18e5d72b2c5
James1011R <31488779+James1011R@users.noreply.github.com>
2020-09-11 01:13:20 -0400

    testing user submitted gradient

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -507,7 +507,7 @@ paper.install(window);
 			},
 			testing: {
 				gradient: {
-					stops: [["rgba(0,0,0,0)", 1], ["rgba(0,0,0,1)", 1]],
+					stops: [["rgba(0,0,0,0)", 0.9], ["rgba(0,0,0,1)", 0.9]],
 					radial: true
 				},
 				origin: path.position,
